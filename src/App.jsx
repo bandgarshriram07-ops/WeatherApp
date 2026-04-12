8d47d2a18196eccfcb06e2850d7557588bc08011
shriram bandgar <bandgarshriram07@gmail.com>
2026-04-12 18:58:54 +0900

    first comment

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Button from '@mui/material/Button';
+import DeleteIcon from '@mui/icons-material/Delete';
+
+function App() {
+  
+
+  return (
+    <>
+      <Button variant="contained" startIcon={<DeleteIcon />}>Contained</Button>
+    </>
+  )
+}
+
+export default App
